57cb88abd6b8a2adc27911835858255ebd6d3d02
ricardotenorio <ricardo.heber@gmail.com>
2021-10-17 12:56:38 -0300

    implemented ProjectListController

---
+++ src/main/java/controller/ProjectListController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import entity.Project;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.ProjectService;
+
+@WebServlet(
+	name = "projectList",
+	urlPatterns = "/"
+	)
+public class ProjectListController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private ProjectService projectService = new ProjectService();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			List<Project> projects = projectService.findAll();
+			RequestDispatcher dispatcher = request.getRequestDispatcher("projects.jsp");
+			
+			request.setAttribute("projects", projects);
+			
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+
+}
